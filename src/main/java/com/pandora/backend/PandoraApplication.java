98e2971eb8f43993f3a2dafd54753f5bdb0a0f68
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-20 19:53:48 -0300

    Including project with Swagger ready

---
+++ src/main/java/com/pandora/backend/PandoraApplication.java
@@ -0,0 +1,11 @@
+package com.pandora.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandoraApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PandoraApplication.class, args);
+	}
+}
