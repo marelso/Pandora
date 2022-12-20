1111d8932d00d423031cb076ac815a8e8714bd6c
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-20 20:57:57 -0300

    Created new entity to handle permission access

--- src/main/java/com/pandora/backend/domain/PermissionType.java
+++ src/main/java/com/pandora/backend/domain/PermissionType.java
@@ -1,2 +1,26 @@
-package com.pandora.backend.domain;public enum PermissionType {
+package com.pandora.backend.domain;
+
+import static com.google.common.collect.ImmutableList.of;
+import static java.util.Collections.emptyList;
+import java.util.List;
+
+public enum PermissionType {
+    SUPER("SUPER", of("EMPLOYEE", "MANAGER", "SUPER")),
+    MANAGER("MANAGER", of("EMPLOYEE", "MANAGER")),
+    EMPLOYEE("EMPLOYEE", of("EMPLOYEE")),
+    NONE("NONE", emptyList());
+
+    private final String permissionId;
+    private final List<String> permissions;
+
+    PermissionType(String permissionId, List<String> permissions) {
+        this.permissionId = permissionId;
+        this.permissions = permissions;
+    }
+
+    public boolean hasPermission(PermissionType permisssion) {
+        return this.permissions
+                .stream()
+                .anyMatch(p -> p.equals(permisssion.permissionId));
+    }
 }
