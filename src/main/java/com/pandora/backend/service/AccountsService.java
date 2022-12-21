af40835bae101292362335f83e1f729430ca7e76
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-20 21:35:14 -0300

    Created new service with crud operations to handle Account processes

--- src/main/java/com/pandora/backend/service/AccountsService.java
+++ src/main/java/com/pandora/backend/service/AccountsService.java
@@ -1,2 +1,38 @@
-package com.pandora.backend.service;public class AccountsService {
+package com.pandora.backend.service;
+
+
+import com.pandora.backend.domain.Accounts;
+import com.pandora.backend.exception.NotFoundException;
+import com.pandora.backend.repository.AccountsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class AccountsService {
+    @Autowired
+    private AccountsRepository repository;
+
+    public List<Accounts> findAll() {
+        return this.repository.findAllByDeletedFalse();
+    }
+
+    public Accounts findById(Integer id) {
+        return this.repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("There is no account with id: ${id}"));
+    }
+
+    public Accounts save(Accounts account) {
+        return this.repository.save(account);
+    }
+
+    public void softDelete(Integer id) {
+        this.repository.softDelete(id);
+    }
+
+    public void delete(Integer id) {
+        this.repository.deleteById(id);
+    }
 }
