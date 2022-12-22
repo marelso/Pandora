335ce5699775c8f870af4b506cf37d29454d3413
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-22 20:00:16 -0300

    Include new auth service to handle authorization process

--- src/main/java/com/pandora/backend/service/auth/AuthService.java
+++ src/main/java/com/pandora/backend/service/auth/AuthService.java
@@ -1,2 +1,31 @@
-package com.pandora.backend.service.auth;public class AuthService {
+package com.pandora.backend.service.auth;
+
+import com.pandora.backend.domain.Account;
+import com.pandora.backend.domain.PermissionType;
+import com.pandora.backend.service.AccountsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    @Autowired
+    private AccountsService accountsService;
+    @Autowired
+    private JwtService jwtService;
+
+    public void authorize(String token, PermissionType permission) {
+        if(!getUser(token).getPermissionType().hasPermission(permission)) {
+            throw new RuntimeException("Feature not allowed for this user.");
+        }
+    }
+
+    private Account getUser(String token) {
+        String username = jwtService.extractUserName(tokenHandler(token));
+
+        return accountsService.loadUserByUsername(username);
+    }
+
+    private String tokenHandler(String token) {
+        return token.substring(7);
+    }
 }
