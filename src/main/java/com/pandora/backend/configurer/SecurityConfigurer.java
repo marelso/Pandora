312d381438c586ea4ac2564db78dd2ccc0a3b8a5
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-21 22:04:27 -0300

    Include new reference to request filter and implementing on configurer

--- src/main/java/com/pandora/backend/configurer/SecurityConfigurer.java
+++ src/main/java/com/pandora/backend/configurer/SecurityConfigurer.java
@@ -1,5 +1,6 @@
 package com.pandora.backend.configurer;
 
+import com.pandora.backend.filters.JwtRequestFilter;
 import com.pandora.backend.service.AccountsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +12,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
     private AccountsService accountsService;
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -29,6 +33,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
