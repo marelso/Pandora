cde4cf1da7f0bef1d423a92c6da20e99e7bf3bd9
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-21 22:02:18 -0300

    Completed request filter to handle authentications

--- src/main/java/com/pandora/backend/filters/JwtRequestFilter.java
+++ src/main/java/com/pandora/backend/filters/JwtRequestFilter.java
@@ -1,2 +1,53 @@
-package com.pandora.backend.filters;public class JwtRequestFilter {
+package com.pandora.backend.filters;
+
+import com.pandora.backend.domain.Account;
+import com.pandora.backend.service.AccountsService;
+import com.pandora.backend.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    @Autowired
+    private AccountsService accountsService;
+    @Autowired
+    private AuthService authService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+            jwt = authHeader.substring(7);
+            username = authService.extractUserName(jwt);
+        }
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            Account userDetails = this.accountsService.loadUserByUsername(username);
+
+            if(authService.validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
