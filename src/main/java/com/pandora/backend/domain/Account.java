36392ce89160ead5a8c1807593eb8bd78433ab87
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-21 21:35:40 -0300

    Change account entity to extends UserDetails from spring security to validate users accounts

--- src/main/java/com/pandora/backend/domain/Account.java
+++ src/main/java/com/pandora/backend/domain/Account.java
@@ -2,14 +2,18 @@ package com.pandora.backend.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Entity
 @Table(name = "accounts")
 @Getter
 @Setter
-public class Account {
+public class Account implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
@@ -20,4 +24,25 @@ public class Account {
     private String lastName;
     private PermissionType permissionType;
     private boolean deleted;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return !this.deleted;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return !this.deleted;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !this.deleted;
+    }
+    @Override
+    public boolean isEnabled() {
+        return !this.deleted;
+    }
 }
