81732a4de6fac19e3854c90ebe75efd89cc45808
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-21 22:53:05 -0300

    Create new entities to handle auth process

---
+++ src/main/java/com/pandora/backend/domain/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.pandora.backend.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class AuthResponse {
+    private final String token;
+}
