43d47415ab59490b741b2f4d4e6bdb05830a2749
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-28 05:08:08 -0300

    Include properties of repository and new method to find workshops actives

--- src/main/java/com/pandora/backend/repository/WorkshopRepository.java
+++ src/main/java/com/pandora/backend/repository/WorkshopRepository.java
@@ -1,2 +1,12 @@
-package com.pandora.backend.repository;public interface WorkshopRepository {
+package com.pandora.backend.repository;
+
+import com.pandora.backend.domain.Workshop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WorkshopRepository extends JpaRepository<Workshop, Integer> {
+    List<Workshop> findAllByDeletedFalse();
 }
