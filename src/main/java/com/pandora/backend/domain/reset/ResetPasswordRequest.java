6cf640418d9f23756b7cdd1685511a934ed26908
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-28 03:34:26 -0300

    Including properties to Reset request entity

--- src/main/java/com/pandora/backend/domain/reset/ResetPasswordRequest.java
+++ src/main/java/com/pandora/backend/domain/reset/ResetPasswordRequest.java
@@ -1,2 +1,12 @@
-package com.pandora.backend.domain.reset;public class ResetPasswordRequest {
+package com.pandora.backend.domain.reset;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResetPasswordRequest {
+    private String name;
+    private String lastName;
+    private String username;
 }
