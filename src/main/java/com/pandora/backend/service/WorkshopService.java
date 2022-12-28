a5704f2d0b529089129c133cd78aaef4d7ea56fa
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-28 05:14:31 -0300

    Include new methods in workshop repository to process entity

--- src/main/java/com/pandora/backend/service/WorkshopService.java
+++ src/main/java/com/pandora/backend/service/WorkshopService.java
@@ -1,2 +1,32 @@
-package com.pandora.backend.service;public class WorkshopService {
+package com.pandora.backend.service;
+
+import com.pandora.backend.domain.Workshop;
+import com.pandora.backend.exception.NotFoundException;
+import com.pandora.backend.repository.WorkshopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WorkshopService {
+    @Autowired
+    private WorkshopRepository repository;
+
+    public List<Workshop> findAll() {
+        return this.repository.findAllByDeletedFalse();
+    }
+
+    public Workshop findById(Integer id) {
+        return this.repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("There is no workshop with id: ${id}"));
+    }
+
+    public Workshop save(Workshop workshop) {
+        return this.repository.save(workshop);
+    }
+
+    public void delete(Integer id) {
+        this.repository.deleteById(id);
+    }
 }
