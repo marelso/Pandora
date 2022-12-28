7e5c066817d74d70a8acfcd6ae0138ef87f98aa9
Marcelo Klein <marcelo.klein@townsq.com.br>
2022-12-28 03:35:50 -0300

    Created new service to handle email processes

--- src/main/java/com/pandora/backend/service/MailService.java
+++ src/main/java/com/pandora/backend/service/MailService.java
@@ -1,2 +1,30 @@
-package com.pandora.backend.service;public class MailService {
+package com.pandora.backend.service;
+
+import com.pandora.backend.domain.Email;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+    @Autowired
+    private JavaMailSender sender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public Email sendEmail(Email email) {
+        var message = new SimpleMailMessage();
+        message.setFrom("klein.marcelojr@gmail.com");
+        message.setTo(email.getTo());
+        message.setText(email.getBody());
+        message.setSubject(email.getSubject());
+
+        email.setFrom(from);
+        sender.send(message);
+
+        return email;
+    }
 }
