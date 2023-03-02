ef90c73c05b9bd5e33e3fe3ef01653227597c54d
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-02 01:56:17 -0300

    Fixing account fixture generation, the id property set was missing. Include new fixture to account dto.

--- src/test/java/com/pandora/backend/fixture/AccountDtoFixture.java
+++ src/test/java/com/pandora/backend/fixture/AccountDtoFixture.java
@@ -1,79 +1,73 @@
 package com.pandora.backend.fixture;
 
-import com.pandora.backend.domain.Account;
 import com.pandora.backend.domain.PermissionType;
+import com.pandora.backend.dto.AccountDto;
+
 import java.util.Random;
 
-public class AccountFixture {
+public class AccountDtoFixture {
     private Integer id;
     private String username;
     private String email;
-    private String password;
     private String name;
     private String lastName;
     private PermissionType permissionType;
     private boolean hasToSetPassword = true;
     private boolean deleted;
 
-    public static AccountFixture anAccountData() {
-        return new AccountFixture();
+    public static AccountDtoFixture anAccountDto() {
+        return new AccountDtoFixture();
     }
 
-    private AccountFixture() {
+    private AccountDtoFixture() {
 
     }
 
-    public AccountFixture withId(Integer id) {
+    public AccountDtoFixture withId(Integer id) {
         this.id = id;
         return this;
     }
 
-    public AccountFixture withUsername(String username) {
+    public AccountDtoFixture withUsername(String username) {
         this.username = username;
         return this;
     }
 
-    public AccountFixture withName(String name) {
+    public AccountDtoFixture withName(String name) {
         this.name = name;
         return this;
     }
 
-    public AccountFixture withEmail(String email) {
+    public AccountDtoFixture withEmail(String email) {
         this.email = email;
         return this;
     }
 
-    public AccountFixture withLastName(String lastName) {
+    public AccountDtoFixture withLastName(String lastName) {
         this.lastName = lastName;
         return this;
     }
 
-    public AccountFixture withPermissionType(PermissionType permissionType) {
+    public AccountDtoFixture withPermissionType(PermissionType permissionType) {
         this.permissionType = permissionType;
         return this;
     }
 
-    public AccountFixture hasToSetPassword(Boolean hasToSetPassword) {
+    public AccountDtoFixture hasToSetPassword(Boolean hasToSetPassword) {
         this.hasToSetPassword = hasToSetPassword;
         return this;
     }
 
-    public AccountFixture withPassword(String password) {
-        this.password = password;
-        return this;
-    }
-
-    public AccountFixture deleted(Boolean deleted) {
+    public AccountDtoFixture deleted(Boolean deleted) {
         this.deleted = deleted;
         return this;
     }
 
-    public Account build() {
-        return new Account(
+    public AccountDto build() {
+        return new AccountDto(
                 id
                 , username
                 , email
-                , password
                 , name
                 , lastName
                 , permissionType
@@ -82,11 +76,10 @@ public class AccountFixture {
         );
     }
 
-    public AccountFixture random() {
+    public AccountDtoFixture random() {
         this.id = new Random().nextInt(100000);
         this.username = "user" + new Random().nextInt(1000);
         this.email = "user" + new Random().nextInt(1000) + "@example.com";
-        this.password = generateRandomPassword();
         this.name = generateRandomName();
         this.lastName = generateRandomLastName();
         this.permissionType = generateRandomPermissionType();
@@ -94,16 +87,6 @@ public class AccountFixture {
         this.deleted = new Random().nextBoolean();
 
         return this;
-    }
-
-    private static String generateRandomPassword() {
-        // generate a random password with 8 characters
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 8; i++) {
-            char c = (char) (new Random().nextInt(26) + 'a'); // generate a random lowercase letter
-            sb.append(c);
-        }
-        return sb.toString();
     }
 
     private static String generateRandomName() {
