41877461adf10abb4f5f0b4b6bf95716be816cde
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-02 01:39:15 -0300

    Include new test to account service tests suite

--- src/test/java/com/pandora/backend/service/AccountsServiceTest.java
+++ src/test/java/com/pandora/backend/service/AccountsServiceTest.java
@@ -1,7 +1,44 @@
 package com.pandora.backend.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.pandora.backend.exception.NotFoundException;
+import com.pandora.backend.repository.AccountsRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
 class AccountsServiceTest {
+    @Mock
+    private AccountsRepository repository;
 
+    @InjectMocks
+    private AccountsService service;
+
+    @Test
+    public void shouldThrowExceptionWhenThereIsNoAccountWithGivenId() {
+        //given
+        final Integer accountId = Integer.valueOf(10);
+
+        given(repository.findById(accountId)).willReturn(Optional.empty());
+
+        //when
+        catchException(() -> service.findById(accountId));
+
+        //then
+        assertThat(caughtException(), instanceOf(NotFoundException.class));
+        then(repository).should().findById(accountId);
+        verifyNoMoreInteractions(repository);
+    }
 }
