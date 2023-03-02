b93ab2f0aad1445dadbec00919ca08ba2d722ba1
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-02 01:57:10 -0300

    Include test to account factory to test dto generation and fixture funcionality.

--- src/test/java/com/pandora/backend/dto/factory/AccountFactoryTest.java
+++ src/test/java/com/pandora/backend/dto/factory/AccountFactoryTest.java
@@ -1,11 +1,18 @@
 package com.pandora.backend.dto.factory;
 
+import com.pandora.backend.domain.PermissionType;
+import com.pandora.backend.fixture.AccountDtoFixture;
+import com.pandora.backend.fixture.AccountFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,6 +23,22 @@ class AccountFactoryTest {
     @Test
     @DisplayName("")
     public void shouldCreateAccountDtoWhenAccountIsActive() {
+        var account = AccountFixture.anAccountData()
+                .random()
+                .deleted(Boolean.FALSE)
+                .build();
 
+        //when
+        var response = subject.from(account);
+
+        //then
+        assertThat(response.getId(), equalTo(account.getId()));
+        assertThat(response.getUsername(), equalTo(account.getUsername()));
+        assertThat(response.getEmail(), equalTo(account.getEmail()));
+        assertThat(response.getName(), equalTo(account.getName()));
+        assertThat(response.getLastName(), equalTo(account.getLastName()));
+        assertThat(response.getPermissionType(), equalTo(account.getPermissionType()));
+        assertThat(response.isHasToSetPassword(), equalTo(account.isHasToSetPassword()));
+        assertFalse(response.isDeleted());
     }
 }
