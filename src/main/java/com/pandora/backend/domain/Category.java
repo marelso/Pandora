851f817f601ed6a9493a52a7dacf9edf3d1fa247
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-07 18:08:09 -0300

    Include entity properties

--- src/main/java/com/pandora/backend/domain/Category.java
+++ src/main/java/com/pandora/backend/domain/Category.java
@@ -1,4 +1,25 @@
 package com.pandora.backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+
+    @ManyToOne
+    private Account includedBy;
+    private boolean deleted;
 }
