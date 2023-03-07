1e649e5189dd1d7f91c4a8b7d715f5d7276d7dd3
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-07 18:45:09 -0300

    Including create exception and delete tests.

--- src/test/java/com/pandora/backend/service/CategoryServiceTest.java
+++ src/test/java/com/pandora/backend/service/CategoryServiceTest.java
@@ -1,14 +1,62 @@
 package com.pandora.backend.service;
 
+import com.pandora.backend.exception.NotFoundException;
 import com.pandora.backend.repository.CategoryRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
     @Mock
     private CategoryRepository repository;
+    @InjectMocks
+    private CategoryService subject;
+
+    @Test
+    public void shouldThrowExceptionWhenThereIsNoAccountWithGivenId() {
+        //given
+        final Integer categoryId = Integer.valueOf(10);
+
+        given(repository.findById(categoryId)).willReturn(Optional.empty());
+
+
+        //when
+        catchException(() -> subject.findById(categoryId));
+
+
+        //then
+        assertThat(caughtException(), instanceOf(NotFoundException.class));
+        then(repository).should().findById(categoryId);
+        verifyNoMoreInteractions(repository);
+    }
+
+    @Test
+    public void shouldDeleteAccountWithGivenId() {
+        //given
+        final Integer categoryId = Integer.valueOf(10);
+
+
+        //when
+        subject.delete(categoryId);
+
+
+        //then
+        then(repository).should().softDelete(categoryId);
+        verify(repository).softDelete(categoryId);
+    }
 }
