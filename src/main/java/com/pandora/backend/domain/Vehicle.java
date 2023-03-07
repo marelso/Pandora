7f516740d20eabde8c37a12f1e4178a1047a9c80
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-07 18:56:35 -0300

    Including annotations

--- src/main/java/com/pandora/backend/domain/Vehicle.java
+++ src/main/java/com/pandora/backend/domain/Vehicle.java
@@ -1,4 +1,19 @@
 package com.pandora.backend.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "vehicles")
 public class Vehicle {
+
 }
