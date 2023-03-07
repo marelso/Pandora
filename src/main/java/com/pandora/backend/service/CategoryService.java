9e00f08ce38e2457dc309c9b9501d6459d710412
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-07 18:20:44 -0300

    Include searching categories methods in category service.

--- src/main/java/com/pandora/backend/service/CategoryService.java
+++ src/main/java/com/pandora/backend/service/CategoryService.java
@@ -1,13 +1,28 @@
 package com.pandora.backend.service;
 
+import com.pandora.backend.domain.Category;
+import com.pandora.backend.exception.NotFoundException;
 import com.pandora.backend.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository repository;
 
+    public List<Category> findAll() {
+        return this.repository.findAllByDeletedFalse();
+    }
 
+    public Category findById(Integer id) {
+        return get(id);
+    }
+
+    private Category get(Integer id) {
+        return this.repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("User", id));
+    }
 }
