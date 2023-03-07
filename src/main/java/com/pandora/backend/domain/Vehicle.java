b9af4727873bba0286f1bdf44e0b9cfefc6cff86
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-07 18:58:36 -0300

    End of development of vehicle entity

--- src/main/java/com/pandora/backend/domain/Vehicle.java
+++ src/main/java/com/pandora/backend/domain/Vehicle.java
@@ -5,8 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -15,5 +14,24 @@ import javax.persistence.Table;
 @AllArgsConstructor
 @Table(name = "vehicles")
 public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String licensePlate;
 
+    @ManyToOne
+    private Account responsibleUser;
+    private String brand;
+    private String carModel;
+    private Integer year;
+    private String color;
+    private String chassis;
+    private VehicleStatus vehicleStatus;
+
+    @Transient
+    private Double efficiency;
+    @Transient
+    private Double totalCost;
+    private String observation;
+    private boolean deleted;
 }
